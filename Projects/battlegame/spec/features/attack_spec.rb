7975283e20adff097a49213155fe66a3f908d98f
Megan Hayes <student@Makerss-MacBook-Air.local>
2020-02-06 16:19:09 +0000

    create attack erb file

--- Projects/battlegame/spec/features/attack_spec.rb
+++ Projects/battlegame/spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'attack' do
   scenario 'player 1 attacks player 2 and gets confirmation' do
     sign_in_and_play
-    player_1.attack(player_2)
+    click_on "Attack"
     expect(page).to have_content "Megan attacked Liz"
   end
 end
